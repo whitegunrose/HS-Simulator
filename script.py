dff81f06d5d1dd8b746e8878c52e47510d3f4949
Brandon Mercado <brandonmercado103@gmail.com>
2023-08-29 20:56:00 -0700

    Create script.py
    Introduced ideas of implementation of various classes, attributes, and methods

---
+++ script.py
@@ -0,0 +1,16 @@
+# Going to create a high-school simulator
+# Classes will include: Student, Teacher, Courses
+
+# 3 attributes and 3 methods for each class
+
+# - Student:
+#   - Attributes: Name (str), Grades (list), Year (int)
+#   - Methods: GoToClass, DoHomework, TakeTest
+
+# - Teacher:
+#   - Attributes: Name (str), Is Friendly (bool), Good Instructor (bool)
+#   - Methods: GiveHomework, GiveTests, GradeWork (both tests and homework)
+
+# - Courses:
+#   - Attributes: Course Name (str), Honors/AP (bool), A-G Requirement (bool) --> Means elective or non/elective
+#   - Methods: ApplyStudentGrade, AddCourse, DropCourse
